76cd254414d6059886318aaa760d616b43627893
Hazialecini <91554788+Hazialecini@users.noreply.github.com>
2021-12-11 18:44:21 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,8 @@
+//
+// Created by hazia lecini on 11/12/21.
+//
 
+#ifndef ESERCITAZIONE_9_LIB_H
+#define ESERCITAZIONE_9_LIB_H
+bool ricerca(char lista[10][20], char nome[1][20], int &pos);
+#endif //ESERCITAZIONE_9_LIB_H
