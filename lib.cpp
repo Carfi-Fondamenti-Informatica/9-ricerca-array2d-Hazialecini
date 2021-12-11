3e6a5be2517700bea48bbdbea487a072ed17b47a
Hazialecini <91554788+Hazialecini@users.noreply.github.com>
2021-12-11 18:44:56 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
-
+//
+// Created by hazia lecini on 11/12/21.
+//
+#include "lib.h"
+#include <cstring>
+bool ricerca(char lista[10][20],char nome[1][20],int &pos){
+    for(int i=0;i<10;i++){
+        if(strcmp(lista[i],nome[0])==0){
+            pos=i;
+            return true;
+        }
+    }return false;
+}
